15667cb4ec6dcad9b1f2b8a8a36aa54ffaa1306a
HarshaPrajapati <harsha_1605@yahoo.com>
2021-01-27 19:44:55 +0530

    connect and creating actions

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {signIn, signOut} from '../actions';
 
 class GoogleAuth extends React.Component{
     state = {
@@ -22,10 +24,13 @@ class GoogleAuth extends React.Component{
             })
         })
     }
-    onAuthChange = () => {
-        this.setState({
-            isSignedIn: this.auth.isSignedIn.get()
-        })
+
+    onAuthChange = (isSignedIn) => {
+        if (isSignedIn){
+            this.props.signIn();
+        } else{
+            this.props.signOut();
+        }
     }
 
     onSignInClick = () => {
@@ -69,6 +74,8 @@ class GoogleAuth extends React.Component{
         )
     }
 }
-
-export default GoogleAuth;
+// const mapStateToProps = () => {
+//     return{}
+// }
+export default connect(null,{signOut,signOut}) (GoogleAuth);
 //hooking up this component to header.js
