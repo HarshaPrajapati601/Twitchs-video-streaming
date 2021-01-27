590af79cc18834446d3f531b7166f71326b55159
HarshaPrajapati <harsha_1605@yahoo.com>
2021-01-27 16:06:43 +0530

    setting a user into auth Flow

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,19 +1,45 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component{
+    state = {
+        isSignedIn: null
+    }
     componentDidMount(){
         window.gapi.load('client:auth2',() => {
             window.gapi.client.init(
                 {
-                    ClientId:'860256405110-nv6rme82m6k1hr46hfa5n9d3nfm4unu3.apps.googleusercontent.com',
+                    clientId:'860256405110-nv6rme82m6k1hr46hfa5n9d3nfm4unu3.apps.googleusercontent.com',
                     scope: 'email'
                 }
-            )
+            ).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                //current authentication status of user if signed in or not
+                this.setState({
+                    state: this.auth.isSignedIn.get()
+                })
+
+            })
         })
+    }
+
+    renderAuthButton(){
+        if(this.state.isSignedIn === null){
+            return(
+                <div>Don't know if signedin or not</div>
+            )
+        }else if(this.state.isSignedIn === true){
+            return(
+                <div>Signed in</div>
+            )
+        }else{
+            return(
+                <div>Not signed in</div>
+            )
+        }
     }
     render(){
         return (
-            <div>googleAuth</div>
+            <div>{this.renderAuthButton()}</div>
         )
     }
 }
